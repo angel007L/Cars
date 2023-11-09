cf8572d88374c8d91ebb6511bbb888e3845d480e
Esiw[Ярослав] <65117234+Esiw12@users.noreply.github.com>
2023-11-10 00:07:52 +0300

    nedo_car

---
+++ priora 3/ClassLibrary1/Class1.cs
@@ -0,0 +1,157 @@
+﻿namespace ClassLibrary1
+{
+    public class Car
+    {
+        public string Brand { get; } = "LADA";
+        public string Model { get; } = "Priora";
+        public string LicensePlate { get; } = "A666YE 777 RUS";
+        public int Speed { get; protected set; } = 0;
+        protected bool EngineRunning = false;
+        public int CurrentGear { get; protected set; } = 0; // Нейтральная передача
+
+        public virtual string CarName => "Lada Priora";
+        public virtual int MaxSpeed => 120;
+
+        public virtual bool Gas()
+        {
+            if (!EngineRunning || CurrentGear == 0)
+            {
+                return false;
+            }
+
+            int increaseAmount = 10;
+            AdjustSpeedByGear(increaseAmount);
+
+            return true;
+        }
+
+        protected void AdjustSpeedByGear(int increaseAmount)
+        {
+            if (CurrentGear == 1 && Speed + increaseAmount > 30) increaseAmount = 30 - Speed;
+            if (CurrentGear == 2 && Speed + increaseAmount > 50) increaseAmount = 50 - Speed;
+            if (CurrentGear == 3 && Speed + increaseAmount > 70) increaseAmount = 70 - Speed;
+            if (CurrentGear == 4 && Speed + increaseAmount > 90) increaseAmount = 90 - Speed;
+            if (CurrentGear == 5 && Speed + increaseAmount > MaxSpeed) increaseAmount = MaxSpeed - Speed;
+
+            Speed += increaseAmount;
+        }
+
+        public bool Brake()
+        {
+            if (Speed == 0)
+            {
+                Console.WriteLine("Скорость нулевая");
+                return false;
+            }
+
+            if (CurrentGear == 0)
+            {
+                Console.WriteLine("Измените передачу");
+                return false;
+            }
+
+            int decreaseAmount = 10;
+            AdjustSpeedByGear(-decreaseAmount);
+
+          
+            if (CurrentGear == 1 && Speed < 0) { Speed = 0; }
+            if (CurrentGear == 2 && Speed < 20) { SetGear(1); }
+            if (CurrentGear == 3 && Speed < 40) { SetGear(2); }
+            if (CurrentGear == 4 && Speed < 60) { SetGear(3); }
+            if (CurrentGear == 5 && Speed < 80) { SetGear(4); }
+
+            return true;
+        }
+
+
+        public virtual string StartEngine()
+        {
+            if (CurrentGear != 0 && CurrentGear != 1)
+            {
+                EngineRunning = false;
+                return $"Двигатель заглох на {CarName}";
+            }
+
+            EngineRunning = true;
+            return $"Двигатель запущен на {CarName} " + " A666YE 777 RUS";
+        }
+
+        public string StopEngine()
+        {
+            EngineRunning = false;
+            return $"Машина {Brand} {Model} заглушена " + " A666YE 777 RUS" ;
+        }
+
+        public bool SetGear(int gear)
+        {
+            if (gear < -1 || gear > 5)
+            {
+                return false; 
+            }
+            if (CurrentGear == 5 && (gear == 3 || gear == 1))
+            {
+                EngineRunning = false; // Глохнем двигатель при таком переключении
+            }
+            // Проверить ограничения скорости для передачи
+            if (gear == -1 && Speed != 0)
+            {
+                return false;
+            }
+
+            CurrentGear = gear;
+            return true;
+        }
+
+        public bool Reverse()
+        {
+            if (!EngineRunning || CurrentGear != 0)
+            {
+                return false;
+            }
+
+            Speed = 0;
+            return true;
+        }
+    }
+
+    public class CarType2 : Car
+    {
+        public override string CarName => "Mazda";
+        public override int MaxSpeed => 150;
+
+        public override bool Gas()
+        {
+            if (!EngineRunning || CurrentGear == 0)
+            {
+                return false;
+            }
+
+            int increaseAmount = 10;
+            AdjustSpeedByGear(increaseAmount);
+
+            return true;
+        }
+        
+    }
+
+    public class CarType3 : Car
+    {
+        public override string CarName => "Porsche 911";
+        public override int MaxSpeed => 200;
+
+        public override bool Gas()
+        {
+            if (!EngineRunning || CurrentGear == 0)
+            {
+                return false;
+            }
+
+            int increaseAmount = 20;
+            AdjustSpeedByGear(increaseAmount);
+
+            return true;
+        }
+
+   
+    }
+}
---
+++ priora 3/YAHAM/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ClassLibrary1;
+
+namespace YAHAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите машину:\r\n1. Priora\r\n2. Mazda\r\n3. Porsche");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int kill = 1;
+            Car car;
+
+            switch (choice)
+            {
+                case 1:
+                    car = new Car();
+                    kill++;
+                    break;
+                case 2:
+                    car = new CarType2();
+                    break;
+                case 3:
+                    car = new CarType3();
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор машины!");
+                    return;
+            }
+
+            // Открыть дверь
+            bool doorOpened = false;
+            bool openDoorWithEffort = false;
+
+            while (!doorOpened)
+            {
+                Console.WriteLine(
+                    "Выберите действие:\r\n1. Открыть дверь нежно. 2. Приложить силы для открытия двери.");
+                int dver = Convert.ToInt32(Console.ReadLine());
+                if (kill == 1)
+                {
+                    if (dver == 1)
+                    {
+                        Console.Write("Дверь открыта ");
+                        doorOpened = true;
+                    }
+                    else
+                    {
+                        Console.Write(("Ты критин, но дверь открыта "));
+                        doorOpened = true;
+                    }
+                }
+                else
+                {
+                    if (dver == 1)
+                    {
+                        Console.WriteLine("Дверь заперта ");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write("Дверь открыта ");
+                        doorOpened = true;
+                    }
+                }
+
+
+                // Остальной код остается без изменений.
+                Console.WriteLine(car.StartEngine());
+
+                bool engineStarted = true;
+                while (engineStarted)
+                {
+                    Console.WriteLine("Выберите действие:");
+                    Console.WriteLine("1. Ускориться");
+                    Console.WriteLine("2. Замедлиться");
+                    Console.WriteLine("3. Заглушить машину");
+                    Console.WriteLine("4. Переключить передачу");
+
+                    int action = Convert.ToInt32(Console.ReadLine());
+                    switch (action)
+                    {
+                        case 1:
+                            if (car.Gas())
+                            {
+                                Console.WriteLine($"Ускорение. Текущая скорость: {car.Speed} км/ч.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Нельзя увеличить скорость на нейтральной передаче.");
+                            }
+
+                            break;
+                        case 2:
+                            if (car.Brake())
+                            {
+                                Console.WriteLine($"Замедление. Текущая скорость: {car.Speed} км/ч.");
+                            }
+
+                            break;
+                        case 3:
+                            if (car is Car)
+                            {
+                                Console.WriteLine("Двигатель LADA Priora заглушен");
+                            }
+                            else if (car is CarType2)
+                            {
+                                Console.WriteLine("Двигатель Mazda заглушен");
+                            }
+                            else if (car is CarType3)
+                            {
+                                Console.WriteLine("Двигатель Porsche заглушен");
+                            }
+
+                            engineStarted = false;
+                            break;
+                        case 4:
+                            Console.WriteLine("На какую передачу хотите переключить?");
+                           
+                            int gear = Convert.ToInt32(Console.ReadLine());
+                            
+            
+                            if (car.SetGear(gear))
+                            {
+                                Console.WriteLine($"Успешно переключено на {gear} передачу.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Нельзя переключить на {gear} передачу при текущей скорости.");
+                            }
+
+                            break;
+                        case 5:
+                            if (car.Reverse())
+                            {
+                                Console.WriteLine("Машина на заднем ходу.");
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
